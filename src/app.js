b087114a1f1c1817733ec59d5bf92507f23057d2
AC <AC@ACs-MacBook-Pro.fios-router.home>
2018-12-25 08:32:33 -0500

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { Provider} from 'react-redux';
 import {addExpense} from './actions/expenses';
 import {setTextFilter} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import { domainToASCII } from 'url';
 
 const store  = configureStore();
 
